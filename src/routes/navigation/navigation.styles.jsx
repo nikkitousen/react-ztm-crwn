f780860a8b80f0b150f387d7068a74b31fae8e25
Nico Bordenabe <nbordenabe@fb.com>
2023-01-23 10:21:01 +0100

    styling

--- src/routes/navigation/navigation.styles.jsx
+++ src/routes/navigation/navigation.styles.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 export const NavigationContainer = styled.div`
   height: 70px;
-  width: 100%;
+  //width: 100%;
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
